c44cc4f6c6cc0d5ec3b543bab5c20853250244da
TowfMinh <vinxen2019@gmail.com>
2024-08-19 15:30:43 +0700

    update server.js

--- api/server.js
+++ api/server.js
@@ -1,32 +1,28 @@
+// server.js
 const jsonServer = require("json-server");
-
 const server = jsonServer.create();
-
-// Uncomment to allow write operations
-const fs = require("fs");
-const path = require("path");
-const filePath = path.join("db.json");
-const data = fs.readFileSync(filePath, "utf-8");
-const db = JSON.parse(data);
-const router = jsonServer.router(db);
-
-// Comment out to allow write operations
-// const router = jsonServer.router("db.json");
-
+const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
-// Add this before server.use(router)
-server.use(
-  jsonServer.rewriter({
-    "/api/*": "/$1",
-    "/blog/:resource/:id/show": "/:resource/:id",
-  })
-);
+server.use(jsonServer.bodyParser);
+
+// Enable CORS for all routes
+server.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE, OPTIONS"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 server.use(router);
+
 server.listen(3000, () => {
   console.log("JSON Server is running");
 });
-
-// Export the Server API
-module.exports = server;
