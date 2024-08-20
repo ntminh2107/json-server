ef8ee1968286454a86d0725b912a1e660506c483
TowfMinh <vinxen2019@gmail.com>
2024-08-20 09:27:00 +0700

    allow crud

--- api/server.js
+++ api/server.js
@@ -1,28 +1,37 @@
-// server.js
+// See https://github.com/typicode/json-server#module
 const jsonServer = require("json-server");
+
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
+const fs = require("fs");
+const path = require("path");
+const db = JSON.parse(fs.readFileSync(path.join("db.json")));
+
+// Uncomment to allow write operations
+// const fs = require('fs')
+// const path = require('path')
+// const filePath = path.join('db.json')
+// const data = fs.readFileSync(filePath, "utf-8");
+// const db = JSON.parse(data);
+// const router = jsonServer.router(db)
+
+// Comment out to allow write operations
+
+const router = jsonServer.router(db);
+
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
-server.use(jsonServer.bodyParser);
-
-// Enable CORS for all routes
-server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, PATCH, DELETE, OPTIONS"
-  );
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
-
+// Add this before server.use(router)
+server.use(
+  jsonServer.rewriter({
+    "/api/*": "/$1",
+    "/blog/:resource/:id/show": "/:resource/:id",
+  })
+);
 server.use(router);
-
 server.listen(3000, () => {
   console.log("JSON Server is running");
 });
+
+// Export the Server API
+module.exports = server;
