febb6370bdab894d6497532a5779f170821523c6
TowfMinh <vinxen2019@gmail.com>
2024-08-20 09:36:52 +0700

    update cors

--- api/server.js
+++ api/server.js
@@ -1,5 +1,6 @@
 // See https://github.com/typicode/json-server#module
 const jsonServer = require("json-server");
+const cors = require("cors");
 
 const server = jsonServer.create();
 const fs = require("fs");
@@ -19,6 +20,7 @@ const db = JSON.parse(fs.readFileSync(path.join("db.json")));
 const router = jsonServer.router(db);
 
 const middlewares = jsonServer.defaults();
+server.use(cors()); // Enable CORS for all origins
 
 server.use(middlewares);
 // Add this before server.use(router)
